6d1c231886bbb787cded30ad05aa7ee6fc34e72b
omiao@outlook.com <omiao@outlook.com>
2015-05-20 13:05:57 +0800

    big change

--- project-create.py
+++ project-create.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- utf-8 -*-
+
 """
 This module is used to create project index file for cscope and ctags.
 It can be used to filter files by their suffix and directory name.
@@ -11,119 +12,159 @@ import os
 import fnmatch
 import argparse
 
-# Help mannul and argument function are created here:
-parser = argparse.ArgumentParser(prog='projcreate', 
-								description=
-								"""
-projcreate is a tool used to create project index file accroding
-with users' requirement. 
-Users can specify which folder are included and not included and
-aslo what kind of files are included and what kinds of not included.
-								""", 
-								epilog = 
-								"""
-This tool is based on python 2.7
-bug report to (gmiao@sonicwall.com)
-								""")#, formatter_class=argparse.RawDescriptionHelpFormatter)
-
-# Now add all the command will be used for this tool
-parser.add_argument('-f', '--folder', nargs='*', help=
-												""" List all the folders that needs to be included
-													in the project with + as prefix and folders that 
-													will not included after it without any prefix
-												""", metavar = 'folders', dest=None)
-parser.add_argument('-t', '--type', nargs='*', help=
-												""" List all file type that will be included in project 
-												""", metavar = 'types', dest='includesuffix')
-parser.add_argument('-e', '--extype', nargs='*', help=
-												""" List all file type that will not be include in project 
-												""", metavar = 'exclude types', dest='excludesuffix')
-parser.add_argument('-a', '--all-others', nargs = '?', help=
-												""" For all other types of files which are not specified will
-												be included in the project
-												""", const = True, default = False, type = bool, metavar = 'include all other types', dest='includedefault')
-args = parser.parse_args()
-print args.folder, args.includesuffix, args.excludesuffix
-
-filedirs = []
-tupledirs = ()
-for dir in args.folder:
-	if '+' in dir:
-		rawdir = dir.strip('+')
-		tupledirs = (rawdir)
-		print rawdir
-	else:
-		pass
 
 # Get the root path that used to index the project
 rootpath = '.'
-
-# Get directory names that used to filer the index file
-filedir = [('firmware/fw', 'firmware/fw/resource'),
-			('firmware/fw/resource/UI5/default/eng', ''),
-			('firmware/gst/gstDictionary.c', ''),
-			('firmware/h', ''),
-			('firmware/bcp', ''),
-			('firmware/libsrc/ip_net2-6.6', '')
-		]
-
-includesuffix = ['txt', 'c']
-excludesuffix = ['png', 'gif']
-
+dirlists = []
+includesuffix = []
+excludesuffix = []
 includedefault = False
-# Get directory names that used to index files
-# includedir = [
-		# 'firmware/fw/resource/UI5/default/eng/',
-		# 'firmware/libsrc/ip_net2-6.6/'
-		# ]
 
-def filter_by_dirname(dir):
-	"""
-	This function is used to filer directory by directory name
-	either with prefix or not
-	"""
-	for includedir, excludedir in filedir:
-		if includedir in dir and (not excludedir or excludedir not in dir):
-			return True
-			
-def filter_by_suffix(filename):
-	"""
-	This function is used to filter filename with a certain suffix which
-	is not needed or only include file with certain suffix.
-	1. If the file extension is in included suffix, then all the file with that
-	extension should be included and others should be excluded.
-	2. If the file extension is in the excluded suffix, then all the file with 
-	that extension should not be included and others should be included.
-	"""
-	for insuffix in includesuffix:
-		extname = '*.' + insuffix
-		if fnmatch.fnmatch(filename, extname):
-			return True
-	for exsuffix in excludesuffix:
-		extname = '*.' + exsuffix
-		if fnmatch.fnmatch(filename, extname):
-			# print '#' + filename
-			return False 
-	
-	if includedefault:
-		return True
-	else:
-		return False
+
+def main():
+    """TODO: Docstring for main.
+    :returns: TODO
+
+    """
+    # Help mannul and argument function are created here:
+    parser = argparse.ArgumentParser(prog='projcreate',
+                                     description="""
+    projcreate is a tool used to create project index file accroding
+    with users' requirement.
+    Users can specify which folder are included and not included and
+    aslo what kind of files are included and what kinds of not included.""",
+                                     epilog="""
+    This tool is based on python 2.7
+    bug report to (gmiao@sonicwall.com)
+                                    """)
+
+    # Now add all the command will be used for this tool
+    parser.add_argument('-f', '--folder', nargs='*', help=""" List all the folders that needs to be included
+                                                        in the project with + as prefix and folders that
+                                                        will not included after it without any prefix
+                                                    """, metavar='folders', dest=None)
+    parser.add_argument('-t', '--type', nargs='*', help=""" List all file type that will be included in project
+                                                    """, metavar='types', dest='insuffix')
+    parser.add_argument('-e', '--extype', nargs='*', help=""" List all file type that will not be include in project
+                                                    """, metavar='exclude types', dest='exsuffix')
+    parser.add_argument('-a', '--all-others', nargs='?', help=""" For all other types of files which are not specified will
+                                                    be included in the project
+                                                    """, const=True, default=False, type=bool, metavar='include all other types', dest='includedefault')
+    args = parser.parse_args()
+
+    dirlist = []
+
+    for folder in args.folder:
+        if '+' in folder:
+            if dirlist:
+                dirlists.append(dirlist)
+                dirlist = []
+                dirlist.append(folder.strip('+'))
+            else:
+                rawdir = folder.strip('+')
+                dirlist.append(rawdir)
+        elif not dirlist:
+            pass
+        else:
+            dirlist.append(folder)
+    dirlists.append(dirlist)
+    # print dirlists
+
+    global includesuffix
+    global excludesuffix
+    global includedefault
+    includesuffix = args.insuffix
+    excludesuffix = args.exsuffix
+    includedefault = args.includedefault
+
+
+def filter_files(filename, filterlist):
+    """TODO: Docstring for filter_files.
+
+    :filename: TODO
+    :filerlist: TODO
+    :returns: TODO
+
+    """
+    for excludedir in filterlist:
+        if excludedir in filename:
+            return False
+    return True
+
+
+def filter_by_dirname(directory):
+    """
+    This function is used to filer directory by directory name
+    either with prefix or not
+    """
+    for diritem in dirlists:
+        includedir = diritem[0]
+        if includedir in directory:
+            # print "\nAdd Folder ", includedir
+            excludedirs = diritem[1:]
+            if excludedirs:
+                # print "\nExclude files the following folders: ", excludedirs
+                for excludedir in excludedirs:
+                    if excludedir in directory:
+                        return False
+            return True
+
+
+def filter_by_suffix(files):
+    """
+    This function is used to filter filename with a certain suffix which
+    is not needed or only include file with certain suffix.
+    1. If the file extension is in included suffix, then all the file with that
+    extension should be included and others should be excluded.
+    2. If the file extension is in the excluded suffix, then all the file with
+    that extension should not be included and others should be included.
+    """
+    if includesuffix:
+        for insuffix in includesuffix:
+            extname = '*.' + insuffix
+            if fnmatch.fnmatch(files, extname):
+                return True
+
+    if excludesuffix:
+        for exsuffix in excludesuffix:
+            extname = '*.' + exsuffix
+            if fnmatch.fnmatch(files, extname):
+                return False
+
+    if includedefault:
+        return True
+    else:
+        return False
+
 
 def test():
-	try:
-		fp = open('cscope.files', 'w+')
-		for dir, dirname, filename in os.walk(rootpath):
-			if filter_by_dirname(dir):
-				for name in filename:
-					if name and filter_by_suffix(name):
-						fp.write(dir + '/' + name + os.linesep)
-						#print dir
-	except:
-		pass
-	finally:
-		fp.close()
-		print "cscope.files has been created......"
+    try:
+        fp = open('cscope.files', 'w+')
+        for dirlist in dirlists:
+            if dirlist[0] != '.':
+                path = rootpath + '/' + dirlist[0]
+            else:
+                path = rootpath
+            filterlist = dirlist[1:]
+            for dir, dirname, filename in os.walk(path):
+                if filename:
+                    for name in filename:
+                        fullpath = dir + '/' + name
+                        if filter_files(fullpath, filterlist) and filter_by_suffix(fullpath):
+                            fp.write(fullpath + os.linesep)
+                            # print fullpath
+    except:
+        print "Error"
+        pass
+    finally:
+        fp.close()
+        print "[+]cscope.files has been created......"
+        print "[+]create cscope index......"
+        os.system("cscope -Rbkq -i cscope.files")
+        print "[+]create ctags......"
+        os.system('ctags -L cscope.files --langmap=c++:+inl --langmap=c++:+txt --langmap=c++:+cfg --langmap=c++:+sym')
 
 if __name__ == '__main__':
-	test()
+    main()
+    test()
+
--- project-create.sh
+++ project-create.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./projcreate.py -f +firmware/fw firmware/fw/resource +firmware/fw/resource/UI5/default/eng +firmware/gst/gstDictionary.c +firmware/h +firmware/bcp +firmware/libsrc/ip_net2-6.6
+./projcreate.py -f +bsp/oct_mips64 +m2 m2/target +firmware/fw firmware/fw/resource +firmware/fw/resource/UI5/default/eng +firmware/gst/gstDictionary.c  +firmware/libsrc/ip_net2-6.6 +firmware/libsrc/openssl-1.0.1e +firmware/ firmware/fw firmware/libsrc firmware/gst  +. bsp firmware m2 SonicPoint misc -t cfg sym dtd txt c cpp mak h csv inc xml html js css 
